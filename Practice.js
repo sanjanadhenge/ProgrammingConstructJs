ef0a157655c44d75edb0e5454ede99cbadf394ff
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-27 17:16:24 +0530

    [sanjana] Add : Calculator

--- Practice.js
+++ Practice.js
@@ -1,8 +1,22 @@
 prompt = require("prompt-sync")();
-let string = prompt('Enter a sentence: ');
-let words = string.split(' ');
-words.sort();
-console.log('The sorted words are:');
-for (const element of words) {
-  console.log(element);
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
 }
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+console.log(`${number1} ${operator} ${number2} = ${result}`);
