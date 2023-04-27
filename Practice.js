b6f1cf0890c0a51b6130d7314d8f4e115d51f9dc
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-27 15:55:14 +0530

    [sanjana] Add :Get Prime Number within range

--- Practice.js
+++ Practice.js
@@ -1,8 +1,24 @@
 prompt = require("prompt-sync")();
-const year = parseInt(prompt('Enter year'));
-if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-    console.log("Leap Year");
-}
-else {
-    console.log("Not leap year");
+const lower = parseInt(prompt('Enter Lower limit'));
+const upper = parseInt(prompt('Enter Upper limit'));
+var flag=0;
+CheckPrime(lower,upper);
+function CheckPrime(lower,upper)
+{
+    for(let i=lower;i<=upper;i++)
+    {
+        for(let j=2;j<=i/2;j++)
+        {
+            if(i%j==0)
+            {
+                flag = 1;
+                break;
+            }
+            flag=0;
+        }
+        if(flag==0)
+        {
+            console.log("\n"+i);
+        }
+    }
 }
