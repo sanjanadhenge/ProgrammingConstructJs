140659b301fcd2eb3f033797a8c54e0277d6b7cd
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-27 17:07:48 +0530

    [sanjana] Add : operations on string

--- Practice.js
+++ Practice.js
@@ -1,10 +1,17 @@
-prompt = require("prompt-sync")();
-var str = String(prompt('Enter string'));
-var str2=" ";
-let a=0;
-for(let i=str.length-1;i>=0;i--)
-{
-    str2[a]=str[i];
-    a++;
-}
-console.log(str2);
+const text1 = 'hello';
+const text2 = 'world';
+const text3 = 'JavaScript';
+const result1 = text1.concat(' ', text2);
+console.log(result1); 
+
+const result2 = text1.toUpperCase();
+console.log(result2); 
+
+const result3 = text3.trim();
+console.log(result3);
+
+const result4 = text1.split();
+console.log(result4); 
+
+const result5= text1.slice(1, 3);
+console.log(result5); 
